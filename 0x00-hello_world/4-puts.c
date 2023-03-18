07f07c9010719990b8f8c7653d265e8b43ddfafc
mnqweneli <qabomnqweneli@gmail.com>
2023-03-18 14:39:37 -0700

    Hello, using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by a new line
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }
