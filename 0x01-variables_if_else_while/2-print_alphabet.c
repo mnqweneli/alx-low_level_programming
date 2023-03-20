010cd60665e82717f118c90e21f39e4157834c8c
mnqweneli <qabomnqweneli@gmail.com>
2023-03-20 13:13:35 -0700

    Print small letter functions using putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Prints the alphabets in lower case
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
