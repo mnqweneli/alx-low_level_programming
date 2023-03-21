a39727059e3c92f926ead8340805520d1c444da7
mnqweneli <qabomnqweneli@gmail.com>
2023-03-20 18:15:11 -0700

    hello using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line
+ * main - prints "Programming is like building a multilingual puzzle,"
  * Return: Always 0 (Success)
  */
 
@@ -10,5 +10,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
