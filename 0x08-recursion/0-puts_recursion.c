8ee058d2793b95a4fad40e9ed156bd444f3bccc2
mnqweneli <qabomnqweneli@gmail.com>
2023-04-05 06:57:33 -0700

    erros fixed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * Author: Mnqweneli Qabo
- * _puts_recursion - fuction
+ * _puts_recursion(char *s) - fuction
  *
  * @s: the input char
  * Return: Always 0.
@@ -13,7 +13,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recurion(s + 1);
+		_puts_recusion(s + 1);
 	}
 	else
 	{
