5ded6924c1699cad803ba6db93aa4bebf2d2fdea
mnqweneli <qabomnqweneli@gmail.com>
2023-04-18 09:24:44 -0700

    struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+GENERAL
+
+Create struct functions for ne data types
+What arae structures ,when, why and how to use them.
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+
+#endif
+
+/**
+ * struct dog - Description,function that calls the new struct data type
+ * @*name: first memeber
+ * @age: second member
+ * @*owner: third memeber
+ *
+ * Description: combines the char and float data items into one
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog;
